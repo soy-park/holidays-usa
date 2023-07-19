771edf68651f655a66979efa01038d84ea75401c
Sara Park <sspark106@gmail.com>
2023-07-19 16:52:45 -0600

    feat: Fetch api data

---
+++ src/apiCalls.js
@@ -0,0 +1,9 @@
+export const getBirds = () => {
+    return fetch("https://date.nager.at/api/v3/publicholidays/2023/US")
+    .then(response => {
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`)
+        }
+          return response.json()
+    })
+}
