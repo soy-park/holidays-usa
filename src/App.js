cc26378f4ef2a6e7cd1ad60b8fc1464cad6c5ae8
Sara Park <sspark106@gmail.com>
2023-07-19 16:54:34 -0600

    feat: Add state of holidays to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import { getBirds } from './apiCalls';
 
-function App() {
+const App = () => {
+  const [holidays, setHolidays] = useState([]);
+
+  useEffect(() => {
+    getBirds() 
+      .then(data => setHolidays(data))
+      .catch(error => console.log('Error fetching data:', error));
+  }, []);
+
   return (
     <div className="App">
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
